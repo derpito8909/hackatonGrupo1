4f22d52502fc4b1e000f40b4fee93e6b064df143
David Rodriguez <davisito544@gmail.com>
2020-09-27 16:00:54 -0500

    creando cards para imagenes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-const cards = document.querySelectorAll('.columna');
+const cards = document.querySelectorAll('.card');
+console.log(cards);
 
-let cartaSeleccionada = false;
+function flip(event) {
+    var element = event.currentTarget;
+    console.log(element)
+    if (element.className === "card col-6") {
+        if (element.style.transform == "rotateY(180deg)") {
+            element.style.transform = "rotateY(0deg)";
+        } else {
+            element.style.transform = "rotateY(180deg)";
+        }
+    }
+};
+
+for (let card of cards) {
+    card.addEventListener('click', flip);
+}
