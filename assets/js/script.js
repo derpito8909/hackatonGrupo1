83973e300c7693e6b7fadf4d13a64eafbc5d57e0
David Rodriguez <davisito544@gmail.com>
2020-09-27 20:05:01 -0500

    ajuste de fotos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,9 @@
 const cards = document.querySelectorAll('.card');
-console.log(cards);
+//console.log(cards);
 
 function flip(event) {
-    var element = event.currentTarget;
-    console.log(element)
-    if (element.className === "card col-6") {
-        if (element.style.transform == "rotateY(180deg)") {
-            element.style.transform = "rotateY(0deg)";
-        } else {
-            element.style.transform = "rotateY(180deg)";
-        }
-    }
+    let element = event.currentTarget;
+    element.className = ''
 };
 
 for (let card of cards) {
