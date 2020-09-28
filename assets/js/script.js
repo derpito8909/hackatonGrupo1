ab9880d2c9dd7511edc8948631a9b4181c56cd6f
David Rodriguez <davisito544@gmail.com>
2020-09-27 23:08:30 -0500

    se agrego la animacion de las cartas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-const cards = document.querySelectorAll('.carta');
-console.log(cards);
+/* En este script se captura cada carta de imagen y se realiza la animacion para voltiar */
+const foto1 = document.getElementById('foto1');
+const foto2 = document.getElementById('foto2');
+const foto3 = document.getElementById('foto3');
+const foto4 = document.getElementById('foto4');
 
 function flip(event) {
     var element = event.currentTarget;
-    console.log(element);
+    console.log(event.originalTarget);
     if (element.className === "carta") {
         if (element.style.transform == "rotateY(180deg)") {
             element.style.transform = "rotateY(0deg)";
@@ -12,7 +15,7 @@ function flip(event) {
         }
     }
 };
-
-for (let card of cards) {
-    card.addEventListener('click', flip);
-}
+foto1.addEventListener('click', flip);
+foto2.addEventListener('click', flip);
+foto3.addEventListener('click', flip);
+foto4.addEventListener('click', flip);
